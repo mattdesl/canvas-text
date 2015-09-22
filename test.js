a39f36f76bb7f8c6f2c60e162416241c2cfdc583
mattdesl <dave.des@gmail.com>
2015-09-22 02:15:46 -0400

    nice text rendering

--- test.js
+++ test.js
@@ -19,14 +19,17 @@ document.body.style.background = 'lightGray'
 document.body.appendChild(canvas)
 
 var chunks = [
-  { text: 'my lazy', size: 128 },
-  { text: ' dog ', size: 42, fillStyle: 'red' },
-  { text: 'jumps over', size: 24 }
+  { text: 'Hello, ', size: 50, fillStyle: 'hsl(100,0%,70%)', weight: 300 },
+  {
+    text: 'world.', size: 32, fillStyle: 'hsl(0,50%,50%)',
+    underline: true, strokeStyle: 'rgba(0,128,255,0.85)', weight: 700
+  },
+  { text: ' This is some canvas text', size: 32, weight: 300 }
 ]
 
 var defaults = {
   baseline: 'alphabetic',
-  align: 'left',
+  align: 'center',
   fillStyle: '#000',
   style: 'normal',
   variant: 'normal',
@@ -37,13 +40,15 @@ var defaults = {
 
 loader(getFontStyle(defaults), function (err) {
   if (err) throw err
+  console.time('render')
   render(context)
+  console.timeEnd('render')
 })
 
 function getFontStyle (opt) {
   var style = opt.style || defaults.style
   var variant = opt.variant || defaults.variant
-  var weight = opt.weight || defaults.weight
+  var weight = String(opt.weight || defaults.weight)
   var family = opt.family || defaults.family
   var fontSize = typeof opt.size === 'number' ? opt.size : defaults.size
   var fontStyle = [ style, variant, weight, fontSize + 'px', family ].join(' ')
@@ -83,59 +88,174 @@ function render (context) {
       attrib.lineHeight = getLineHeight(attrib.size)
     }
 
-    // gather metrics if we don't have any
-    if (!attrib.metrics || typeof attrib.metrics.width !== 'number') {
-      setFontParams(attrib)
-      attrib.metrics = context.measureText(attrib.text)
-    }
-
     return attrib
   })
 
-  var fullText = getFullText(data)
+  var fullText = composeBuffer(data)
   var lines = wordWrap.lines(fullText, {
-    width: 200,
+    width: 500,
     measure: measure
   })
 
-  console.log(lines)
-
+  var originX = 50
+  var originY = 50
   var cursorX = 0
   var cursorY = 0
-  data.forEach(function (attrib) {
-    var text = attrib.text
-    var start = attrib.start || 0
-    var end = typeof attrib.end === 'number' ? attrib.end : text.length
-    var substr = text.substring(start, end)
-    var lineHeight = attrib.lineHeight
-    var metrics = attrib.metrics
 
-    var x = cursorX
-    var y = cursorY + (maxHeight - lineHeight)
-    setFontParams(attrib)
-    context.fillStyle = attrib.fillStyle
-    context.fillText(substr, x, y + lineHeight)
+  var maxLineWidth = lines.reduce(function (prev, line) {
+    return Math.max(line.width, prev)
+  }, 0)
 
-    context.beginPath()
-    context.lineTo(x, y + lineHeight)
-    context.lineTo(x + metrics.width, y + lineHeight)
-    context.stroke()
+  lines.forEach(function (line, i, list) {
+    var start = line.start
+    var end = line.end
+    var lineWidth = line.width
+    var maxAttrHeight = getMaxAttrHeight(data, start, end)
 
-    cursorX += metrics.width
+    while (start < end) {
+      // find next attribute chunk for this string char
+      var attribIdx = indexOfAttribute(data, start)
+      if (attribIdx === -1) {
+        break
+      }
+
+      // for each attribute in this range ...
+      var attrib = data[attribIdx]
+      var offset = start - attrib.index
+
+      var length = Math.min(attrib.text.length - offset, end - start)
+      var text = fullText.substring(start, start + length)
+      var lineHeight = attrib.lineHeight
+
+      var x = originX + cursorX
+      var y = originY + cursorY + (maxHeight - lineHeight)
+      if (defaults.align === 'right') {
+        x += (maxLineWidth - lineWidth)
+      } else if (defaults.align === 'center') {
+        x += (maxLineWidth - lineWidth) / 2
+      }
+
+      setFontParams(attrib)
+      var textWidth = context.measureText(text).width
+      cursorX += textWidth
+      context.fillStyle = attrib.fillStyle
+
+      if (attrib.underline) {
+        context.beginPath()
+        context.strokeStyle = attrib.strokeStyle || attrib.fillStyle
+        context.lineWidth = 4
+        context.lineCap = 'round'
+        context.lineJoin = 'round'
+        var offY = y + 4
+        context.lineTo(x, offY + lineHeight)
+        context.lineTo(x + textWidth, offY + lineHeight)
+        context.stroke()
+      }
+
+      context.fillText(text, x, y + lineHeight)
+
+      // skip to next chunk
+      start = attrib.index + attrib.text.length
+      maxAttrHeight = Math.max(maxAttrHeight, attrib.lineHeight)
+    }
+    cursorX = 0
+    cursorY += maxAttrHeight
+
+    var nextLine = list[i + 1]
+    if (nextLine) {
+      var nextLineHeight = getMaxAttrHeight(data, nextLine.start, nextLine.end)
+      cursorY += nextLineHeight - maxAttrHeight
+    }
   })
 
   function measure (text, start, end, width) {
-    var chunkIndex = 0
-    var available = 0
-    for (var i = start; i < end; i++) {
+    var availableGlyphs = 0
+    var first = start
+    var totalWidth = 0
+
+    for (; start < end; start++) {
+      // find next attribute chunk for this string char
+      var attribIdx = indexOfAttribute(data, start)
+      if (attribIdx === -1) {
+        break
+      }
+
+      // for each attribute in this range ...
+      var attrib = data[attribIdx]
+      var result = computeAttributeGlyphs(context, fullText, totalWidth, attrib, start, end, width)
+      availableGlyphs += result.available
+      totalWidth += result.width
+
+      // skip to next attribute
+      start += attrib.text.length - 1
+    }
+
+    return {
+      width: totalWidth,
+      start: first,
+      end: first + availableGlyphs
     }
   }
+
 }
 
-function getFullText (data) {
-  return data.reduce(function (prev, attrib) {
-    return prev + attrib.text
-  }, '')
+function getMaxAttrHeight (data, start, end) {
+  var maxAttrHeight = 0
+  // first we need to compute max attribute height for this line
+  while (start < end) {
+    // find next attribute chunk for this string char
+    var attribIdx = indexOfAttribute(data, start)
+    if (attribIdx === -1) {
+      break
+    }
+
+    // for each attribute in this range ...
+    var attrib = data[attribIdx]
+    maxAttrHeight = Math.max(attrib.lineHeight, maxAttrHeight)
+    start += attrib.text.length
+  }
+  return maxAttrHeight
+}
+
+function computeAttributeGlyphs (context, fullText, prevWidth, attrib, start, end, width) {
+  var text = attrib.text
+  var length = Math.min(text.length, end - start)
+
+  // determine how many chars in this chunk can be shown
+  setFontParams(attrib)
+  for (var off = 0; off < length; off++) {
+    var substr = fullText.substring(start, start + (length - off))
+    var newWidth = context.measureText(substr).width
+    if ((prevWidth + newWidth) <= width) {
+      return { available: substr.length, width: newWidth }
+    }
+  }
+  return { available: 0, width: 0 }
+}
+
+function indexOfAttribute (data, charIndex) {
+  // find the first attribute at this character index
+  for (var i = 0; i < data.length; i++) {
+    var attrib = data[i]
+    var text = attrib.text
+    var length = text.length
+    if (charIndex >= attrib.index && charIndex < attrib.index + length) {
+      return i
+    }
+  }
+  return -1
+}
+
+function composeBuffer (data) {
+  var buffer = ''
+  var previous = 0
+  data.forEach(function (attrib) {
+    var text = attrib.text
+    buffer += text
+    attrib.index = previous
+    previous = attrib.index + text.length
+  })
+  return buffer
 }
 
 function loader (font, cb) {
@@ -150,3 +270,23 @@ function loader (font, cb) {
     cb(new Error('font loading not supported'))
   }
 }
+
+// simple
+// data.forEach(function (attrib) {
+//   var text = attrib.text
+//   var lineHeight = attrib.lineHeight
+//   var metrics = attrib.metrics
+
+//   var x = cursorX
+//   var y = cursorY + (maxHeight - lineHeight)
+//   setFontParams(attrib)
+//   context.fillStyle = attrib.fillStyle
+//   context.fillText(text, x, y + lineHeight)
+
+//   context.beginPath()
+//   context.lineTo(x, y + lineHeight)
+//   context.lineTo(x + metrics.width, y + lineHeight)
+//   context.stroke()
+
+//   cursorX += metrics.width
+// })
